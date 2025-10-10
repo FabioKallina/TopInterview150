62e875cc2061e8730bba2a2a1b78f2614fc0bef8
FabioKallina <fabiokdp8@gmail.com>
2025-10-10 16:08:04 -0700

    Arrays&Strings

---
+++ 01_Arrays&Strings/JumpGame.js
@@ -0,0 +1,34 @@
+
+/**
+You are given an integer array nums. You are initially positioned at the array's first index, 
+and each element in the array represents your maximum jump length at that position.
+Return true if you can reach the last index, or false otherwise.
+
+Example 1:
+
+Input: nums = [2,3,1,1,4]
+Output: true
+Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+
+Example 2:
+
+Input: nums = [3,2,1,0,4]
+Output: false
+Explanation: You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it impossible to reach the last index.
+*/
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canJump = function(nums) {
+    let maxReachable = 0;
+    for ( let i = 0; i < nums.length; i++ ) {
+        if ( i > maxReachable ) return false;
+        maxReachable = Math.max(maxReachable, i + nums[i]);
+    }
+    return true;
+};
+/** Time and Space Complexity
+ * Time: O(n)
+ * Space: O(1)
+ */
