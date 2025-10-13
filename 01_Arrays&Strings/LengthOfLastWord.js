c13a8cfdf43421f06eb6a537239b26db118f38ed
FabioKallina <fabiokdp8@gmail.com>
2025-10-13 15:19:01 -0700

    Arrays&Strings

---
+++ 01_Arrays&Strings/LengthOfLastWord.js
@@ -0,0 +1,39 @@
+
+/**
+Given a string s consisting of words and spaces, return the length of the last word in the string.
+A word is a maximal substring consisting of non-space characters only.
+
+Example 1:
+
+Input: s = "Hello World"
+Output: 5
+Explanation: The last word is "World" with length 5.
+
+Example 2:
+
+Input: s = "   fly me   to   the moon  "
+Output: 4
+Explanation: The last word is "moon" with length 4.
+
+Example 3:
+
+Input: s = "luffy is still joyboy"
+Output: 6
+Explanation: The last word is "joyboy" with length 6.
+*/
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+    let newStr = s.trim();
+    let count = 0;
+    for ( let i = newStr.length - 1; i >= 0; i++ ) {
+        if ( newStr[i] === " " ) return count;
+        count++;
+    }
+};
+/** Time and Space Complexity
+ * Time: O(n)
+ * Space: O(1)
+ */
