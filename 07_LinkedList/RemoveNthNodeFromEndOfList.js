3552aeb89b4756eb9b0e054379713cbe666fa20d
FabioKallina <fabiokdp8@gmail.com>
2025-10-20 17:14:14 -0700

    LinkedList

---
+++ 07_LinkedList/RemoveNthNodeFromEndOfList.js
@@ -0,0 +1,77 @@
+
+/**
+Given the head of a linked list, remove the nth node from the end of the list and return its head.
+
+Example 1:
+
+Input: head = [1,2,3,4,5], n = 2
+Output: [1,2,3,5]
+
+Example 2:
+
+Input: head = [1], n = 1
+Output: []
+
+Example 3:
+
+Input: head = [1,2], n = 1
+Output: [1]
+*/
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+var removeNthFromEnd = function(head, n) {
+    let dummy = new ListNode(0, head);
+    let curr = dummy, prev = dummy;
+    while (n > 0) {
+        curr = curr.next;
+    }
+    while (curr.next) {
+        curr = curr.next;
+        prev = prev.next;
+    }
+    prev.next = prev.next.next;
+    return dummy.next;
+};
+/** Time and Space Complexity
+ * Time: O(n)
+ * Space: O(1)
+ */
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/** Without Dummy
+ * 
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+var removeNthFromEnd = function(head, n) {
+    let len = 0;
+    let temp = head;
+    while (temp) {
+        len++;
+        temp = temp.next;
+    }
+    if ( n === len ) return head.next;
+    let curr = head;
+    for ( let i = 0; i < len - n - 1; i++ ) {
+        curr = curr.next;
+    }
+    curr.next = curr.next.next;
+    return head;
+};
---
+++ 07_LinkedList/ReverseLinkedList2.js
@@ -0,0 +1,48 @@
+
+/**
+Given the head of a singly linked list and two integers left and right where left <= right, 
+reverse the nodes of the list from position left to position right, and return the reversed list.
+
+Example 1:
+
+Input: head = [1,2,3,4,5], left = 2, right = 4
+Output: [1,4,3,2,5]
+
+Example 2:
+
+Input: head = [5], left = 1, right = 1
+Output: [5]
+*/
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} left
+ * @param {number} right
+ * @return {ListNode}
+ */
+var reverseBetween = function(head, left, right) {
+    if ( !head || left === right ) return head;
+    let dummy = new ListNode(0, head);
+    let prev = dummy;
+    for ( let i = 0; i < left - 1; i++ ) {
+        prev = prev.next;
+    }
+    let curr = prev.next;
+    for ( let i = 0; i < right - left; i++ ) {
+        let temp = curr.next;
+        curr.next = temp.next;
+        temp.next = prev.next;
+        prev.next = temp;
+    }
+    return dummy.next;
+};
+/** Time and Space Complexity
+ * Time: O(n)
+ * Space: O(1)
+ */
